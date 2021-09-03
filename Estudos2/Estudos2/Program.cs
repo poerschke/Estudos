69a7b6e1320f0f29e892b1ca81754bb24a0d2335
Douglas Poerschke Rocha <douglas.poerschke@gmail.com>
2021-09-03 09:50:14 -0300

    Add files via upload

---
+++ Estudos2/Estudos2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estudos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ primeiro/primeiro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "douglas";
+            int idade = 35;
+            double variavel = 4658.465;
+            Console.WriteLine("{0} tem {1} anos e tem saldo de R$ {2:F2}", nome, idade, variavel);
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo de R$ {variavel:F2}");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo de R$ " + variavel.ToString("F2"));
+        }
+    }
+}
